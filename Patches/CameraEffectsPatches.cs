47bd28eb857cc1d761809c7371469b69dabd0cae
moosemeat817 <michaelsd123456@gmail.com>
2026-03-10 09:44:37 -0500

    Add files via upload

---
+++ Patches/CameraEffectsPatches.cs
@@ -0,0 +1,13 @@
+﻿namespace RecordingUtils.Patches
+{
+	//[HarmonyPatch(typeof(CameraEffects), nameof(CameraEffects.Update))]
+	//public class CameraEffectsUpdatePatch
+	//{
+	//	public static void Postfix()
+	//	{
+	//		if (Settings.RequiresBloomReset)
+	//			GameManager.GetCameraEffects()
+	//				.BloomEnable(Settings.ModSettings.EnableBloom);
+	//	}
+	//}
+}
---
+++ Patches/InputManagerPatches.cs
@@ -0,0 +1,30 @@
+﻿using HarmonyLib;
+using Il2Cpp;
+using RecordingUtils.Commands;
+using RecordingUtils.FreeBird;
+using UnityEngine;
+
+namespace RecordingUtils.Patches
+{
+	[HarmonyPatch(typeof(InputManager), "ProcessInput")]
+	public class InputManagerProcessInputPatch
+	{
+		public static bool Prefix(ref InputManager __instance)
+		{
+			if (FBCam.Instance == null)
+				return true;
+
+			return !FBCam.Instance.Enabled;
+		}
+
+		public static void Postfix(ref InputManager __instance)
+		{
+			if (Input.GetKeyDown(Settings.ModSettings.Freeze))
+				uConsole.print(CommandList.CmdAnimalFreeze.Execute());
+
+			if (Input.GetKeyDown(Settings.ModSettings.Wander))
+				uConsole.print(CommandList.CmdAnimalWanderToMyLocation.Execute());
+		}
+	}
+
+}
